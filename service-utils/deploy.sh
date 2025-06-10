41b6e6752ab04613150f413ff70bb26b10bc76af
SparklitWallet <215305021+SparklitWallet@users.noreply.github.com>
2025-06-10 13:56:24 +0500

    Delete service-utils directory

--- service-utils/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Deploying SparklitWallet..."
-docker-compose down
-docker-compose build
-docker-compose up -d
-echo "Deployment complete."
--- service-utils/install_deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-npm install
-echo "All dependencies installed."
